9f58404507863cc69cb72a037098169dc6b6fbc5
mvryens <michiel.vryens@acerta.be>
2023-04-28 14:43:23 +0200

    Pushing my internship files !

---
+++ Javascript/main.js
@@ -0,0 +1,20 @@
+// Declaring variables of different data types
+
+let first = 'Michel'
+let last = 'Larian'
+let land = 'Denmark'
+let stad = 'Aars'
+let leeftijd = '69'
+let isMarried = false
+
+// Declaring variables with number values
+
+const gravity = 9.81, boilingPoint = 100
+let name = 'MV', werk = 'Student', live = "Aars"
+
+// Using those variables
+
+console.log(first, last, land, stad)
+console.log(gravity, boilingPoint)
+console.log(name, werk, live)
+console.log('I am ' + leeftijd + ' years old ! And I live in a town called ' + stad + ' in ' + land)
---
+++ Javascript/playingWithMath.js
@@ -0,0 +1,19 @@
+// Using some Math objects
+
+const PI = Math.PI
+const randNum = Math.random()
+
+console.log(PI)
+console.log(Math.round(PI))
+console.log(Math.round(9.81))
+console.log(Math.floor(PI))
+console.log(Math.ceil(PI))
+console.log(Math.min(-5, 3, 20, 4, 5, 10))
+console.log(Math.max(-5, 3, 20, 4, 5, 10))
+console.log(randNum)
+console.log(Math.floor(randNum * 11))
+console.log(Math.abs(-10))
+console.log(Math.sqrt(100))
+console.log(Math.sqrt(2))
+console.log(Math.pow(3, 2))
+console.log(Math.E)
